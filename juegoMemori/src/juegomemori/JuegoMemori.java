5d9577fea358285e4b231c24d5cd3ff26cde25b6
kathsup <katherinencarvallo22@gmail.com>
2024-12-08 12:11:19 -0600

    Add files via upload

---
+++ juegoMemori/src/juegomemori/JuegoMemori.java
@@ -0,0 +1,30 @@
+package juegomemori;
+
+import java.util.Random;
+
+public class JuegoMemori {
+
+public int []getCardNumbers(){
+
+   int[] numbers = new int[16];
+   int count = 0; 
+   
+   while(count<16){
+   Random r = new Random(); 
+   int na = r.nextInt(8)+1;
+   int nvr =0; 
+   
+       for (int i = 0; i < 16; i++) {
+           if(numbers[i]==na){
+           nvr++; 
+           }
+       }
+   if(nvr<2){
+   numbers[count]=na;
+   count++;
+   }
+   
+   }
+  return numbers; 
+}
+}
